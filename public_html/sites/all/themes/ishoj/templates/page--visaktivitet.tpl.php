48a60df4f72b2c0e7e4ab6eb135d7009ee4c2474
riivobellcom <riivo.erik@bellcom.ee>
2018-01-12 12:47:32 +0200

    Made several parts of the footer columns editable (additional changes)

--- public_html/sites/all/themes/ishoj/templates/page--visaktivitet.tpl.php
+++ public_html/sites/all/themes/ishoj/templates/page--visaktivitet.tpl.php
@@ -381,11 +381,7 @@
               <?php print render($page['footer_sociale']); ?>
               <h3>Følg os på sociale medier</h3>
               <p>
-                <a class="sprite sprite-facebook footer" href="https://www.facebook.com/ishojnaturcenter" title="Følg Ishøj Naturcenter på Facebook"><span><span class="screen-reader">Følg Ishøj Naturcenter på Facebook</span></span></a>
-                <!-- <a class="sprite sprite-facebook footer" href="http://www.facebook.com/ishojkommune" title="Følg Ishøj Kommune på Facebook"><span><span class="screen-reader">Følg Ishøj Kommune på Facebook</span></span></a>
-                <a class="sprite sprite-linkedin footer" href="https://www.linkedin.com/company/ishoj-kommune" title="Følg Ishøj Kommune på LinkedIn"><span><span class="screen-reader">Følg Ishøj Kommune på LinkedIn</span></span></a>
-                <a class="sprite sprite-twitter footer" href="http://www.twitter.com/ishojkommune" title="Følg Ishøj Kommune på Twitter"><span><span class="screen-reader">Følg Ishøj Kommune på Twitter</span></span></a>
-                <a class="sprite sprite-youtube footer" href="http://www.youtube.com/tvishoj" title="Følg Ishøj Kommune på Youtube"><span><span class="screen-reader">Følg Ishøj Kommune på Youtube</span></span></a> -->
+                <?php print $social_media_links; ?>
               </p>
             </div>
             <div class="grid-third">
